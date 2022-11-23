1aab6848d1d0e1700f0fcafc6d0f0c0b84b6945f
Franco Aguilera <flaalf@gmail.com>
2022-11-23 14:18:08 -0300

    CG request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Angular-CoinGecko';
+export class AppComponent implements OnInit {
+  BASE = 'https://api.coingecko.com/';
+  PATH = 'api/v3/coins/markets?';
+  PARAMS =
+    'vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const URL = `${this.BASE}${this.PATH}${this.PARAMS}`;
+    this.http.get(URL).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
