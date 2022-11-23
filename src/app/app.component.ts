c556365eb7b724cc9e0c5694c75e87c8635e74c0
Franco Aguilera <flaalf@gmail.com>
2022-11-23 15:09:35 -0300

    table style and list filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface ICoin {
+  id: string;
+  symbol: string;
+  name: string;
+  image: string;
+  current_price: number;
+  price_change_percentage_24h: number;
+  market_cap: number;
+  total_volume: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,17 +23,43 @@ export class AppComponent implements OnInit {
   PARAMS =
     'vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
 
+  titles: string[] = [
+    '#',
+    'Coin',
+    'Price',
+    'Price Change',
+    'Market Cap',
+    'Total Volume',
+  ];
+
+  searchText = '';
+  coins: ICoin[] = [];
+  filteredCoins: ICoin[] = [];
+
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     const URL = `${this.BASE}${this.PATH}${this.PARAMS}`;
-    this.http.get(URL).subscribe(
+    this.http.get<ICoin[]>(URL).subscribe(
       (res) => {
-        console.log(res);
+        this.coins = res;
+        this.filteredCoins = res;
       },
       (err) => {
         console.log(err);
       }
+    );
+  }
+
+  searchCoin() {
+    this.filteredCoins = this.coins.filter(
+      (coin) =>
+        coin.name
+          .toLocaleLowerCase()
+          .includes(this.searchText.toLocaleLowerCase()) ||
+        coin.symbol
+          .toLocaleLowerCase()
+          .includes(this.searchText.toLocaleLowerCase())
     );
   }
 }
